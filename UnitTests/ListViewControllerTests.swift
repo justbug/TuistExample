7fee5b35e78f7a6d948abf3acc962c69bd096141
Mark Chen <lazzyjessie@gmail.com>
2021-11-15 21:24:18 +0800

    Add ListViewControllerTests

---
+++ UnitTests/ListViewControllerTests.swift
@@ -0,0 +1,32 @@
+//
+//  ListViewControllerTests.swift
+//  ListAppUnitTests
+//
+//  Created by Mark Chen on 2021/11/15.
+//
+
+import XCTest
+
+class ListViewControllerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
